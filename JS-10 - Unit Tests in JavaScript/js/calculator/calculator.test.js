9d883d8bf8a95a9f6c784fb0dcc5c09156dccde7
Julius Aboagye <juliusaboagye45@gmail.com>
2020-06-02 15:19:04 +0100

    New Commit

--- JS-10 - Unit Tests in JavaScript/js/calculator/calculator.test.js
+++ JS-10 - Unit Tests in JavaScript/js/calculator/calculator.test.js
@@ -15,3 +15,16 @@ test('subtract 5 - 4 to equal 1', () => {
 test('subtract -5 - -10 to equal 5', () => {
   expect(calculator.subtract(-5, -10)).toBe(5);
 });
+test('divide 10 by 5 answer should be 2', () => {
+  expect(calculator.divide(10, 5)).toBe(2);  
+});
+
+test('divide 300 by 50 answer should be 2', () => {
+  expect(calculator.divide(300, 50)).toBe(6);
+});
+test('multiply 10 by 5 answer should be 50', () => {
+  expect(calculator.multiply(10, 5)).toBe(50);
+});
+test('multiply 1.5 by 2 answer should be 3', () => {
+  expect(calculator.multiply(1.5, 2)).toBe(3);
+});
--- JS-10 - Unit Tests in JavaScript/js/user/user-controller.test.js
+++ JS-10 - Unit Tests in JavaScript/js/user/user-controller.test.js
@@ -16,4 +16,41 @@ test('remove user to userController', () => {
     expect(userController.users).not.toContain(user);
   });
 
+  //ONE
+  test('Verifies user before adding', () => {    
+    let user = new User(7324,"Julius", "juliusaboagye45@gmail.com");
+    expect(userController.getUsers()).not.toContain(user);
+    userController.add(user);    
+  });
+
+//Two
+  test('verify and remove', () => {    
+    let user = new User(1234,"Charlie", "santiago@generation.org");
+    expect(userController.users).not.toContain(user);
+    userController.remove(user);
+  });
+
+  //THREE
+  test('Find user by email', () => {    
+    let user = new User(1234,"Santiago", "santiago@generation.org");
+    let testEmail="santiago@generation.org";
+    expect(userController.findByEmail(testEmail)).toEqual(user);
+   
+  });
+
+
+  test('Find user by email', () => {    
+    let user = new User(7324,"Julius", "juliusaboagye45@gmail.com");
+    let testEmail="juliusaboagye45@gmail.com";
+    expect(userController.findByEmail(testEmail)).toEqual(user);
+   
+  });
+
+  //FOUR
+  test('Find user by id', () => {    
+    
+    let user = new User(1234,"Santiago", "santiago@generation.org");
+    let testId=1234;
+    expect(userController.findById(testId)).toEqual(user);
+  });
 
